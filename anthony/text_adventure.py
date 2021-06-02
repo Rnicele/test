c1f4bc468805b166192b002ceb650b90c7fa83a2
Mark Anthony Posadas <manthonyposadas@gmail.com>
2021-06-02 13:46:43 +0800

    WIP: text adventure

---
+++ anthony/text_adventure.py
@@ -0,0 +1,92 @@
+story = {
+    'statement': 'On your way to your grand mother\'s home, you encounter a wolf in the forest.',
+    'choices': [
+        {
+            'key': 'a',
+            'label': 'Run.',
+            'statement': 'You ran away from the wolf.',
+            'choices': [
+                {
+                    'key': 'a',
+                    'label': 'Run towards the village to look for the villagers.',
+                    'statement': 'You\'re running.',
+                    'choices': [
+                         {
+                             'key': 'a',
+                             'label': 'Give up.',
+                             'statement': 'LOSE: You\'ve given up finding someone to help you, the wolf catched up and eaten you alive.',
+                         },
+                        {
+                             'key': 'b',
+                             'label': 'Continue running.',
+                             'statement': 'WIN: You found a villager and got help.',
+                         },
+                    ]
+                },
+                {
+                    'key': 'b',
+                    'label': 'Run to your home to hide.',
+                    'statement': 'You reached your home.',
+                    'choices': [
+                        {
+                            'key': 'a',
+                            'label': 'Tell your mother what\'s happening.',
+                            'statement': 'WIN: You told what happened to your mother and got help from villagers.',
+                        },
+                        {
+                            'key': 'b',
+                            'label': 'Keep it to yourself.',
+                            'statement': 'LOSE: The wolf followed you and killed the unknowing villagers including you and your mother.',
+                        },
+                    ]
+                },
+                {
+                    'key': 'c',
+                    'label': 'Run towards grand mother\'s house.',
+                    'statement': 'You reached your grand mother\'s house.',
+                    'choices': [
+                        {
+                            'key': 'a',
+                            'label': 'Look for hunters nearby.',
+                            'statement': 'WIN: You found a hunter and he killed wolf.',
+                        },
+                        {
+                            'key': 'b',
+                            'label': 'Hide together with your grand mother.',
+                            'statement': 'LOSE: The wolf found and killed both of you.',
+                        },
+                    ]
+                }
+            ]
+        },
+        # 'b': 'Converse with the wolf.',
+    ]
+}
+
+
+def process(part: dict):
+    """
+    Process a story recursively.
+
+    Keyword arguments:
+    part -- the part of story (dict)
+    """
+
+    print(part['statement'])
+
+    if not 'choices' in part:
+        return
+
+    for choice in part['choices']:
+        print(f"[{choice['key']}] {choice['label']}")
+
+    answer = input('Choice: ')
+
+    print('\n')
+
+    process(
+        next((item for item in part['choices'] if item['key'] == answer), None)
+    )
+
+
+process(story)
