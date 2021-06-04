56abc3efb4614327ceda6f25dcd99aabaca6bb88
Mark Anthony Posadas <manthonyposadas@gmail.com>
2021-06-04 12:54:27 +0800

    WIP calculator

--- anthony/calculator.py
+++ anthony/calculator.py
@@ -1,23 +1,87 @@
-import tkinter.font as tkFont
 from tkinter import *
+from tkinter.ttk import *
 
-root = Tk()
 
-frame = Frame(root, width=600, height=0)
-frame.pack()
+class Calculator:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title('Calculator')
+        self.root.resizable(False, False)
+        self.equation = StringVar()
+        self.total = ''
+        self.irrigate()
+        self.root.mainloop()
 
-font = tkFont.Font(family='Helvetica', size=16, weight='normal')
-result_font = tkFont.Font(family='Helvetica', size=22, weight='bold')
+    def irrigate(self):
+        Entry(
+            self.root,
+            textvariable=self.equation,
+            font=('Segeo UI', 20, 'bold')
+        ).grid(
+            columnspan=5,
+            ipady=18
+        )
 
-canvas_width = 600
-canvas_height = 440
+        buttons = [
+            ['+', 1, 4, 1],
+            ['-', 2, 4, 1],
+            ['*', 3, 4, 1],
+            ['/', 4, 4, 1],
+            ['%', 1, 3, 1],
+            ['+/-', 1, 2, 1],
+            ['C', 1, 1, 1],
+            ['.', 5, 1, 1],
+            ['=', 5, 3, 2, 38],
+            ['0', 5, 2, 1],
+            ['1', 2, 1, 1],
+            ['2', 2, 2, 1],
+            ['3', 2, 3, 1],
+            ['4', 3, 1, 1],
+            ['5', 3, 2, 1],
+            ['6', 3, 3, 1],
+            ['7', 4, 1, 1],
+            ['8', 4, 2, 1],
+            ['9', 4, 3, 1],
+        ]
 
-w = canvas_width // 2
-h = canvas_height // 2
+        for button in buttons:
+            self.make(*button)
 
-canvas = Canvas(root, width=canvas_width, height=canvas_height)
-canvas.pack()
-canvas.create_rectangle(30, 30, canvas_width-30, canvas_height, fill='grey')
-canvas.create_text(w, 50, text="RED RIDING HOOD", fill='white', font=font)
+    def make(self, label: str, row: int, column: int, column_span: int, ipadx=0):
+        Button(
+            self.root,
+            text=label,
+            command=lambda arg=label: self.evaluate(arg)
+        ).grid(
+            row=row,
+            column=column,
+            columnspan=column_span,
+            ipadx=ipadx,
+            ipady=15,
+            sticky=W,
+        )
 
-# canvas.create_text(w-30, h+30, text=introduction, fill='white', justify='center', font=introFont)
+    def evaluate(self, sign: str):
+        if sign == 'C':
+            self.equation.set('')
+            return
+
+        if (sign == '+/-'):
+            return
+
+        if (sign == '.') and ('.' in self.equation.get()):
+            return
+
+        if sign == '=':
+            try:
+                result = eval(self.equation.get())
+                self.equation.set("{:.2f}".format(result))
+            except SyntaxError:
+                pass
+
+            return
+
+        self.equation.set(f"{self.equation.get()}{sign}")
+
+
+Calculator()
